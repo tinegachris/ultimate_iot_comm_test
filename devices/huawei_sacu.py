ac2a22147b4bc016a70017a062d91c6c2ec3d61a
tingachris <chrispinetinega@gmail.com>
2024-07-23 18:34:43 +0300

    Reading SN from sacu device

--- devices/huawei_sacu.py
+++ devices/huawei_sacu.py
@@ -6,18 +6,18 @@
 
 
 ## Importing required modules.
-from comm_protocols import ModbusTcpTest
-from tools import Timer
-from tools import Logger
+from comm_protocols.modbus_tcp_test import ModbusTcpTest
+from tools.timer import Timer
+from tools.logger import Logger
 
 # HuaweiSacu ip address.
-ip_address = "192.168.0.5"
+sacu_ip_address = "192.168.0.5"
 
 # HuaweiSacu port number.
 port_number = 502
 
 # HuaweiSacu slave id.
-slave_id = 0
+sacu_slave_id = 0
 
 # HuaweiSacu sub_devices slave ids.
 sacu_pcs_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -33,12 +33,57 @@ bms_list = []
 for i in range(0, len(sacu_bms_id)):
     bms_list.append("BMS" + str(sacu_bms_id[i]))
 
-# HuaweiSacu registers list.
-sacu_serial_number_register = 40713
+# HuaweiSacu registers list
+sacu_serial_num_reg = 40713 # string type, 10 registers
 
 
 # This class is used to test Huawei SACU device.
 class test_HuaweiSacu:
     def __init__(self):
-        pass
+        # Create a ModbusTcpTest object.
+        self.modbus = ModbusTcpTest(sacu_ip_address, port_number)
+        # Create a Timer object.
+        self.timer = Timer()
+        # Create a Logger object.
+        self.logger = Logger()
+    
+    # initiate connection to HuaweiSacu device.
+    def connect(self):
+        # Connect to the HuaweiSacu device.
+        self.modbus.connect()
+        
+    # close connection to HuaweiSacu device.
+    def disconnect(self):
+        # Close the connection to the HuaweiSacu device.
+        self.modbus.disconnect()
+        
+    # Read the serial number of HuaweiSacu device.
+    def read_serial_num(self):
+        # Read the serial number of HuaweiSacu device.
+        serial_num = self.modbus.read_string(sacu_slave_id, sacu_serial_num_reg, 10)
+        return serial_num
 
+if __name__ == "__main__":
+    # Create a test_HuaweiSacu object.
+    test_sacu = test_HuaweiSacu()
+    # Connect to the HuaweiSacu device.
+    
+    test_sacu.connect()
+    
+    if test_sacu.modbus.is_connected() == True:
+        Logger.info("Connection to HuaweiSacu device is successful.")
+        # Read the serial number of HuaweiSacu device.
+        Logger.info("Reading the serial number of HuaweiSacu device...")
+        # Start the timer.
+        test_sacu.timer.start()
+        # Read the serial number of HuaweiSacu device.
+        serial_num = test_sacu.read_serial_num()
+        # Stop the timer.
+        test_sacu.timer.stop()
+        # Display the serial number of HuaweiSacu device.
+        Logger.info("Serial number of HuaweiSacu device: " + serial_num)
+        # Display the time taken to read the serial number.
+        Logger.info("Time taken to read the serial number: " + str(test_sacu.timer.elapsed_time()) + " seconds.")
+    
+    # Close the connection to the HuaweiSacu device.
+    test_sacu.disconnect()
